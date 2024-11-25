933c0199da79ed8855d12cf1bb56c54841ae2e6a
Roy Roodenburg <royroodtje@gmail.com>
2024-11-25 22:03:55 +0100

    Added logic to the script on deploy to execute the script given to it

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const io = require('socket.io-client');
+const fs = require('fs');
+const { exec } = require('child_process');
 
 const token = process.env.AGENT_KEY;
 const socket = io(process.env.HOST, {
-    query: {token}
+    query: { token }
 });
-
+console.log(process.env.AGENT_KEY, process.env.HOST)
 // Handle connection
 socket.on('connect', () => {
     console.log('Connected to the Socket.IO server');
@@ -18,5 +20,24 @@ socket.on('disconnect', () => {
 
 // handle version status
 socket.on(`deploy-version-${token}`, (data) => {
-    console.log('version-status', data);
+    fs.writeFile('script.js', data.script, (err) => {
+        if (err) throw err;
+        console.log('script.js created.');
+
+        // Step 2: Run the script
+        exec('node script.js', (err, stdout, stderr) => {
+            if (err) {
+                console.error(`Execution error: ${err}`);
+                return;
+            }
+
+            console.log(`Output from script.js: ${stdout}`);
+
+            // Step 3: Remove the script file
+            fs.unlink('script.js', (err) => {
+                if (err) throw err;
+                console.log('script.js removed.');
+            });
+        });
+    });
 });
