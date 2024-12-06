f3d13f7968976343e8ed9ab0033e44a5ce28415e
Roy Roodenburg <royroodtje@gmail.com>
2024-12-06 21:11:44 +0100

    Added TS to the agent

---
+++ src/server.ts
@@ -0,0 +1,42 @@
+const io = require("socket.io-client");
+const fs = require("fs");
+const { exec } = require("child_process");
+
+const token = process.env.AGENT_KEY;
+const socket = io(process.env.HOST, {
+  query: { token },
+});
+console.log(process.env.AGENT_KEY, process.env.HOST);
+// Handle connection
+socket.on("connect", () => {
+  console.log("Connected to the Socket.IO server");
+});
+
+// Handle disconnection
+socket.on("disconnect", () => {
+  console.log("Disconnected from the server");
+});
+
+// handle version status
+socket.on(`deploy-version-${token}`, (data: any) => {
+  fs.writeFile("script.js", data.script, (err: any) => {
+    if (err) throw err;
+    console.log("script.js created.");
+
+    // Step 2: Run the script
+    exec("node script.js", (err: any, stdout: any) => {
+      if (err) {
+        console.error(`Execution error: ${err}`);
+        return;
+      }
+
+      console.log(`Output from script.js: ${stdout}`);
+
+      // Step 3: Remove the script file
+      fs.unlink("script.js", (err: any) => {
+        if (err) throw err;
+        console.log("script.js removed.");
+      });
+    });
+  });
+});
